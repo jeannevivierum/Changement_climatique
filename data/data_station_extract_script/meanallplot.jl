54e55be3e51a619c41483cd94e094d29e4615117
Mario-Lapi <145753293+Mario-Lapi@users.noreply.github.com>
2024-04-19 18:40:13 +0200

    code pour decomposer les ST

---
+++ data/data_station_extract_script/meanallplot.jl
@@ -0,0 +1,26 @@
+using Dates, DataFrames, DataFramesMeta, StatsPlots, Forecast
+
+# Charger les données
+df = CSV.read("data_tx/TX_STAID000032.txt", DataFrame, skipto = 22, header = 21, comment="#", dateformat = "yyyymmdd", types=Dict(:DATE => Date), normalizenames=true, ignoreemptyrows=true)
+
+# Filtrer les données entre 1970 et 2006
+df_filtered = filter(row -> year(row.DATE) >= 1970 && year(row.DATE) <= 2006, df)
+
+# Conversion factor to °C
+factor = 0.1
+
+# Calculer la moyenne et l'écart type pour chaque date
+df_daily = @chain df_filtered begin
+    @subset(:Q_TX .!= 9) # Supprimer les valeurs manquantes 
+    @transform(:YEAR = year.(:DATE)) # Ajouter une colonne pour l'année
+    @by(:DATE, :DAILY_MEAN = mean(:TX)*factor, :DAILY_STD = std(:TX)*factor) # Grouper par DATE et prendre la moyenne / écart type
+end
+
+# Convertir les données en une série temporelle
+ts = TimeArray(df_daily.DAILY_MEAN, df_daily.DATE)
+
+# Décomposer la série temporelle
+decomposition = decompose(ts)
+
+# Tracer les composantes de la décomposition
+plot(decomposition)
---
+++ data/data_station_extract_script/plotter.jl
@@ -0,0 +1,16 @@
+using Dates, DataFrames, DataFramesMeta
+
+
+df = CSV.read("Donnees_tx/TX_STAID000032.txt", DataFrame, skipto = 22, header = 21, comment="#",dateformat = "yyyymmdd", types=Dict(:DATE => Date), normalizenames=true, ignoreemptyrows=true)
+
+using DataFramesMeta, StatsBase
+factor = 0.1 # conversion factor to °C
+df_month = @chain df begin
+    @subset(:Q_TX .!= 9) # remove missing 
+    @transform(:MONTH = month.(:DATE)) # add month column
+    @by(:MONTH, :MONTHLY_MEAN = mean(:TX)*factor, :MONTHLY_STD = std(:TX)*factor) # grouby MONTH + takes the mean/std in each category 
+end
+
+using StatsPlots
+@df df_month plot(monthabbr.(1:12), :MONTHLY_MEAN, ribbon = :MONTHLY_STD, label = "Mean Temperature")
+ylabel!("T(°C)")
