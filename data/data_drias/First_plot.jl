9c39e6ed0c2564b558473f4166bbbb4dabbf6c5e
Mario-Lapi <145753293+Mario-Lapi@users.noreply.github.com>
2024-04-29 11:32:10 +0200

    modif temporal.jl

--- data/data_drias/First_plot.jl
+++ data/data_drias/First_plot.jl
@@ -3,4 +3,3 @@ using CSV, Dates, DataFrames
 df = CSV.read("data/data_drias/donnees/Bastia.txt",DataFrame, header = 1, comment="#", dateformat = "yyyymmdd", types=Dict(:Date => Date), normalizenames=true)
 
 df_filtered = filter(row -> year(row.Date) >= 1970 && year(row.Date) <= 2006, df)
-
