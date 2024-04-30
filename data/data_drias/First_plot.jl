2c5789c4bd9611b798e5845db70e1282adfd8649
jeannevivierum <145753305+jeannevivierum@users.noreply.github.com>
2024-04-30 16:05:55 +0200

    premiers graph

--- data/data_drias/First_plot.jl
+++ data/data_drias/First_plot.jl
@@ -1,5 +1,15 @@
-using CSV, Dates, DataFrames
+using CSV, Dates, DataFrames, DataFramesMeta, Statistics
 
 df = CSV.read("data/data_drias/donnees/Bastia.txt",DataFrame, header = 1, comment="#", dateformat = "yyyymmdd", types=Dict(:Date => Date), normalizenames=true)
 
 df_filtered = filter(row -> year(row.Date) >= 1970 && year(row.Date) <= 2006, df)
+
+df_month = @chain df begin
+    @subset(:Tmoy .!= 9) # remove missing 
+    @transform(:MONTH = month.(:Date)) # add month column
+    @by(:MONTH, :MONTHLY_MEAN = mean(:Tmoy), :MONTHLY_STD = std(:Tmoy)) # grouby MONTH + takes the mean/std in each category 
+end
+
+using StatsPlots
+@df df_month plot(monthabbr.(1:12), :MONTHLY_MEAN, ribbon = :MONTHLY_STD, label = "Mean Temperature")
+ylabel!("Temperature(°C)")
