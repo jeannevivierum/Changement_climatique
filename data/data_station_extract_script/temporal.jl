a3de0999efb2b8cc25e84d0068c9974f128d63ca
Mario-Lapi <145753293+Mario-Lapi@users.noreply.github.com>
2024-05-17 18:26:27 +0200

    Analyse des résidus

--- data/data_station_extract_script/temporal.jl
+++ data/data_station_extract_script/temporal.jl
@@ -1,21 +1,42 @@
 using CSV
 using DataFrames
+using DataFramesMeta
 using Dates
 using StatsPlots
 using Statistics
 using Temporal
 using GLM
-using DataFramesMeta
+using TimeSeries: TimeArray 
+using StatsBase
 using Forecast
 
 # Charger les données
-df = CSV.read("data_tx/LYON.txt", DataFrame, skipto = 22, header = 21, comment="#", dateformat = "yyyymmdd", types=Dict(:DATE => Date), normalizenames=true)
+df = CSV.read("data_station_extract_script/data_tx/MONTPELLIER.txt", DataFrame, skipto = 22, header = 21, comment="#", dateformat = "yyyymmdd", types=Dict(:DATE => Date), normalizenames=true)
+df_drias = CSV.read("data_drias/donnees/Montpellier.txt",DataFrame, header = 60, comment="#", dateformat = "yyyymmdd", types=Dict(:Date => Date), normalizenames=true)
 
-# Filtrer les données entre 1970 et 2006
-df_filtered = filter(row -> year(row.DATE) >= 1951 && year(row.DATE) <= 2020, df)
+# Filtrer les données entre 1951 et 2006
+df_filtered = filter(row -> year(row.DATE) >= 1951 && year(row.DATE) <= 2005, df)
+df_filtered_drias = filter(row -> year(row.Date) >= 1951 && year(row.Date) <= 2005, df_drias)
 
 # Conversion factor to °C
 factor = 0.1
+
+df_month = @chain df_filtered begin
+    @subset(:Q_TX .!= 9) # remove missing 
+    @transform(:MONTH = month.(:DATE)) # add month column
+    @by(:MONTH, :MONTHLY_MEAN = mean(:TX)*factor, :MONTHLY_STD = std(:TX)*factor) # grouby MONTH + takes the mean/std in each category 
+end
+df_month_drias = @chain df_filtered_drias begin
+    @subset(:Tmax .!= 9) # remove missing 
+    @transform(:MONTH = month.(:Date)) # add month column
+    @by(:MONTH, :MONTHLY_MEAN = mean(:Tmax), :MONTHLY_STD = std(:Tmax)) # grouby MONTH + takes the mean/std in each category 
+end
+
+
+@df df_month plot(monthabbr.(1:12), :MONTHLY_MEAN, ribbon = :MONTHLY_STD, label = "Températures observées")
+ylabel!("Temperature(°C)")
+@df df_month_drias plot!(monthabbr.(1:12), :MONTHLY_MEAN, ribbon = :MONTHLY_STD, label = "Températures DRIAS")
+ylabel!("Temperature(°C)")
 
 # Calculer la moyenne et l'écart type pour chaque date
 df_daily = @chain df_filtered begin
@@ -23,7 +44,29 @@ df_daily = @chain df_filtered begin
     @transform(:YEAR = year.(:DATE)) # Ajouter une colonne pour l'année
     @by(:DATE, :DAILY_MEAN = mean(:TX)*factor, :DAILY_STD = std(:TX)*factor) # Grouper par DATE et prendre la moyenne / écart type
 end
+# Calculer la moyenne et l'écart type pour chaque date
+df_daily_drias = @chain df_filtered_drias begin
+    @subset(:Tmax .!= 9) # Supprimer les valeurs manquantes 
+    @transform(:YEAR = year.(:Date)) # Ajouter une colonne pour l'année
+    @by(:Date, :DAILY_MEAN = mean(:Tmax), :DAILY_STD = std(:Tmax)) # Grouper par DATE et prendre la moyenne / écart type
+end
+# Convertir les données en une série temporelle avec TimeSeries
+my_ts = TimeArray(df_daily.DATE, df_daily.DAILY_MEAN)
+my_ts_drias = TimeArray(df_daily_drias.Date, df_daily_drias.DAILY_MEAN)
 
+# Extraire les valeurs numériques de la série temporelle
+data_values = values(my_ts)
+data_values_drias = values(my_ts_drias)
+
+# Calculer l'autocorrélogramme partiel avec un nombre maximal de décalages spécifié
+maxlag = 50 # Définir le nombre maximal de décalages
+lags = collect(0:maxlag) # Créer une plage de décalages
+pacf_values = StatsBase.pacf(data_values, lags)
+pacf_values_drias = StatsBase.pacf(data_values_drias, lags)
+
+plot(0:maxlag, pacf_values, xlabel="Lag", ylabel="Partial Autocorrelation", title="autocorrélogramme partiel", markershape=:circle, line=:stem)
+plot!(0:maxlag, pacf_values_drias, xlabel="Lag", ylabel="Partial Autocorrelation", title="autocorrélogramme partiel", markershape=:circle, line=:stem)
+hline!([0], color=:black, linewidth=1.5)
 
 stl_df = stl(df_daily,365; robust=true, spm=true)
 stl_df.decomposition
@@ -124,4 +167,23 @@ plot(df_daily.DATE, seasonal_values, label="Saisonnalité", xlabel="Date", ylabe
 
 
 
+
+
+function moyenne_gauss(data::Vector{T}, window_size::Int) where T
+    n = length(data)
+    trend = similar(data, T)
+    for i in 1:n
+        weights = exp.(-(collect(1:window_size) .- (window_size ÷ 2)).^2 / (2 * (window_size / 2)^2))
+        weights /= sum(weights)  # Normalisation des poids pour que leur somme soit égale à 1
+        start_idx = max(1, i - window_size ÷ 2)
+        end_idx = min(n, start_idx + window_size - 1)  # Ajustement de la fin de la tranche de données
+        data_slice = @view(data[start_idx:end_idx])
+        trend[i] = sum(data_slice .* weights[1:length(data_slice)])
+    end
+    return trend
+end
+
+# Calculer la tendance par moyenne mobile avec une fenêtre de taille 730 (deux ans)
+trend_gauss = moyenne_gauss(df_daily.DAILY_MEAN, 730)
+trend_gauss_drias = moyenne_gauss(df_daily_drias.DAILY_MEAN, 730)
 
