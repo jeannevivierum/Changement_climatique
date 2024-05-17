fbde6ec670e73148b1e843bb0f010dbe81e94d13
jeannevivierum <145753305+jeannevivierum@users.noreply.github.com>
2024-05-17 14:33:20 +0200

    petits test AR

--- data/data_drias/decompose.jl
+++ data/data_drias/decompose.jl
@@ -5,3 +5,29 @@ stl_co2.decomposition
 using StatsPlots
 # je préfère plotter moi même je n'aime pas trop les plots tout integrés du genre plot (stl co2)*
 @df stl_co2.decomposition plot(:Timestamp, :Seasonal)
+
+
+# Test arima
+
+
+using Forecast
+using Random
+
+# Générer des données simulées pour un modèle AR(2)
+Random.seed!(123)  # Pour la reproductibilité
+n = 100
+phi = [0.5, -0.25]  # Coefficients AR
+sigma = 1.0  # Écart-type du bruit blanc
+data = Vector{Float64}(undef, n)
+data[1:2] = randn(2)  # Initialiser les deux premières valeurs
+
+for i in 3:n
+    data[i] = phi[1] * data[i-1] + phi[2] * data[i-2] + sigma * randn()
+end
+
+# Ajuster un modèle AR(2) aux données
+p = 2  # Ordre du modèle AR
+model = arfit(data, p)
+
+# Afficher les coefficients estimés
+println("Coefficients estimés du modèle AR: ", model.arparams)
